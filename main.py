4fe96c2476c1ba5f7fcb085000101eccaf099930
PriskySimbar <jofanprisky72@gmail.com>
2026-08-14 12:03:44 +0800

    prepare churn api for cloud deployment

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load("churn_pipeline.pkl")
+
+THRESHOLD = 0.4033179352921264
+
+
+class Customer(BaseModel):
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
+
+
+@app.get("/")
+def root():
+    return {"message": "Churn Prediction API is running"}
+
+
+@app.post("/predict")
+def predict(customer: Customer):
+
+    data = pd.DataFrame([customer.model_dump()])
+
+    probability = model.predict_proba(data)[0][1]
+
+    prediction = "Yes" if probability >= THRESHOLD else "No"
+
+    return {
+        "prediction": prediction,
+        "churn_probability": round(float(probability), 4),
+        "threshold": THRESHOLD
+    }
